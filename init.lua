30b2123da611462b1373d831365772e2026678c5
Raymundo Alva <rayalva407@gmail.com>
2025-08-16 00:26:05 -0500

    refactors file structure

--- init.lua
+++ init.lua
@@ -1,25 +1,4 @@
 require 'core.options'
 require 'core.keymaps'
-require 'plugins.lsp'
-
--- Lazy.nvim install
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end
-
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
-
-require('lazy').setup({
-  require 'plugins.neotree',
-  require 'plugins.colortheme',
-  require 'plugins.lualine',
-  require 'plugins.treesitter',
-  require 'plugins.telescope',
-  require 'plugins.whichkey'
-})
+require 'core.lazy'
+require 'core.lsp'
